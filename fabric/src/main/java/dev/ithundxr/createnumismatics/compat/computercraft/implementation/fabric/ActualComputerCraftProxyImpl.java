b619d3d433b45159f6e40ac761d536da38798eb1
blendtermimal <155683938+blendtermimal@users.noreply.github.com>
2026-06-10 16:19:25 -0400

    Register Fabric CC peripherals directly

--- fabric/src/main/java/dev/ithundxr/createnumismatics/compat/computercraft/implementation/fabric/ActualComputerCraftProxyImpl.java
+++ fabric/src/main/java/dev/ithundxr/createnumismatics/compat/computercraft/implementation/fabric/ActualComputerCraftProxyImpl.java
@@ -4,21 +4,27 @@ import dan200.computercraft.api.peripheral.PeripheralLookup;
 import dev.ithundxr.createnumismatics.compat.computercraft.ComputerCraftProxy;
 import dev.ithundxr.createnumismatics.compat.computercraft.implementation.ComputerBehaviour;
 import dev.ithundxr.createnumismatics.compat.computercraft.implementation.peripherals.BankTerminalPeripheral;
+import dev.ithundxr.createnumismatics.compat.computercraft.implementation.peripherals.BrassDepositorPeripheral;
+import dev.ithundxr.createnumismatics.compat.computercraft.implementation.peripherals.VendorPeripheral;
+import dev.ithundxr.createnumismatics.content.depositor.BrassDepositorBlockEntity;
+import dev.ithundxr.createnumismatics.content.vendor.VendorBlockEntity;
 import dev.ithundxr.createnumismatics.registry.NumismaticsBlocks;
-import dev.ithundxr.createnumismatics.util.Utils;
-import net.minecraft.core.registries.Registries;
 
 public class ActualComputerCraftProxyImpl {
     public static void registerWithDependency() {
         ComputerCraftProxy.computerFactory = ComputerBehaviour::new;
 
-        PeripheralLookup.get().registerFallback((level, blockPos, blockState, blockEntity, direction) ->
-            ComputerBehaviour.peripheralProvider(level, blockPos)
-        );
+        PeripheralLookup.get().registerFallback((level, blockPos, blockState, blockEntity, direction) -> {
+            if (blockEntity instanceof BrassDepositorBlockEntity brassDepositor)
+                return new BrassDepositorPeripheral(brassDepositor);
+            if (blockEntity instanceof VendorBlockEntity vendor)
+                return new VendorPeripheral(vendor);
+            return ComputerBehaviour.peripheralProvider(level, blockPos);
+        });
 
-        Utils.runOnceRegistered(Registries.BLOCK, () -> PeripheralLookup.get().registerForBlocks(
+        PeripheralLookup.get().registerForBlocks(
             (world, pos, state, blockEntity, context) -> BankTerminalPeripheral.INSTANCE,
             NumismaticsBlocks.BANK_TERMINAL.get()
-        ));
+        );
     }
 }
